091a7fbfdbc99788e855a7604ffbe6ff4f34765f
Martin Plaza <martinplaza321@gmail.com>
2024-04-22 15:08:54 -0300

    primer commit

---
+++ practica preparcial.py
@@ -0,0 +1,21 @@
+"""Ejercicio 1
+Para cada empleado de una empresa es necesario evaluar sus conocimientos técnicos y
+determinar si un empleado cumple con los estándares de capacitación que la empresa se ha
+propuesto. Un empleado con por lo menos un curso por cada 6 meses de antigüedad cumple
+con los estándares propuestos por la empresa.
+Se conoce la cantidad de empleados a evaluar con una antigüedad mayor a 6 meses, son 46.
+De cada empleado se conoce: nombre - legajo - cursos realizados - antigüedad en meses.
+Mostrar un menú principal con las opciones:
+1. Registrar empleados
+Se cargarán todos los empleados de una vez. Para cada empleado, se cargan los
+siguientes datos: nombre, legajo y antigüedad en meses. Se debe validar que el legajo
+tenga 5 carácteres de longitud.
+2. Agregar nuevo curso
+Se ingresa el legajo del empleado y se lo busca. En caso de que el legajo no sea
+encontrado mostrar un cartel aclaratorio. Si el empleado es encontrado se solicita se
+escriba el nombre del curso realizado para agregar en la lista del empleado
+cursos_realizados.
+3. Mostrar resumen
+Se muestra un resumen cómo sigue, ordenado de mayor a menor por cantidad de
+cursos realizados."""
+
